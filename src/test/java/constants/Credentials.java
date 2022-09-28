bf4d38d8ccca06f24dcf1bc9526c8b02ee7d0dcd
Andrei <Andrei>
2022-09-28 08:50:55 +0300

    Add Credentials

---
+++ src/test/java/constants/Credentials.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class Credentials {
+
+    public static final String EMAIL = "andrei92d@gmail.com";
+
+    public static final String PASSWORD = "jU@GYxu$bNPCX7R";
+
+    public static final String TOKEN = "1782db64feaa92201230b9d35123059cfd14d1d2";
+}
+
